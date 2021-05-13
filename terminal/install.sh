84d06a66bacc186e6bf886ce870c982056f2ac5d
Nicolas Beauvais <nicolas@bvs.email>
2021-05-13 22:20:30 +0100

    Fix Prezto install

--- terminal/install.sh
+++ terminal/install.sh
@@ -3,13 +3,22 @@
 #
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
 
-for rcfile in "$HOME"/.dotfiles/terminal/prezto/runcoms/*(.); do
+# Install Prezto contrib
+git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/belak/prezto-contrib "$HOME/.zprezto/contrib"
+
+for rcfile in "$HOME/.dotfiles/terminal/prezto/runcoms/*(.)"; do
     ln -s "$rcfile" "$HOME/.${rcfile:t}"
 done
 
-for module in "$HOME"/.dotfiles/terminal/prezto/modules/*; do
+for module in "$HOME/.dotfiles/terminal/prezto/modules/*"; do
     ln -s "$module" "$HOME/.zprezto/modules/${module:t}"
 done
 
+# Remove invalid profile for which2
+sudo rm /etc/profile.d/which2.sh
+
 # Set Zsh as default shell
 chsh -s $(which zsh)
+
+# Setup Kitty configuration
+ln -s "$HOME/.dotfiles/terminal/kitty" "$HOME/.config/kitty"
