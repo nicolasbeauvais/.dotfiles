f891eddadcbc1dec3d4ac194a41e539edc338177
Nicolas Beauvais <nicolas@bvs.email>
2021-05-13 22:19:48 +0100

    Add high memory limit for phpstan alias

--- terminal/prezto/modules/php/init.zsh
+++ terminal/prezto/modules/php/init.zsh
@@ -3,7 +3,7 @@
 #
 alias phpspec='./vendor/bin/phpspec'
 alias phpunit='./vendor/bin/phpunit'
-alias phpstan='./vendor/bin/phpstan'
+alias phpstan='php -d memory_limit=2G ./vendor/bin/phpstan'
 alias phpcs='./vendor/bin/phpcs'
 alias phpcbf='./vendor/bin/phpcbf'
 alias sail='./vendor/bin/sail'
