228c30316d2cd7647f500051ed7ae4a9502a8caf
Nicolas Beauvais <nicolas@bvs.email>
2021-05-13 22:19:28 +0100

    Fix repository clone link

--- clone.sh
+++ clone.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 PRIVATE_REPOSITORY=https://nicolasbeauvais@github.com/nicolasbeauvais/.private.git
-PUBLIC_REPOSITORY=git@github.com:nicolasbeauvais/.dotfiles.git
+PUBLIC_REPOSITORY=https://github.com/nicolasbeauvais/.dotfiles.git
 
 git clone "$PRIVATE_REPOSITORY" ~/.private
-git clone "PUBLIC_REPOSITORY" ~/.dotfiles
+git clone "$PUBLIC_REPOSITORY" ~/.dotfiles
 
 bash ~/.private/install.sh
 bash ~/.dotfiles/install.sh
