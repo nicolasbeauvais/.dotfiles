404c0cacb961b38c012baab88aef601fa4252126
Nicolas Beauvais <nicolas@bvs.email>
2024-11-06 11:17:32 +0100

    Add Jetbrains toolbox scripts to path

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -9,6 +9,7 @@ set PATH /usr/local/{bin,sbin} $PATH
 set PATH ~/.local/bin $PATH
 set PATH ~/.node_modules/bin $PATH
 set PATH ~/.config/composer/vendor/bin $PATH
+set PATH ~/.local/share/JetBrains/Toolbox/scripts $PATH
 set PATH /snap/bin $PATH
 
 # Pyenv
